696de30dcf4f5537d85a9afd41a86cbd0d6be980
Smit Patel <smitpatel0045@gmail.com>
2023-12-17 18:34:59 -0400

    init

---
+++ src/main/java/com/smitpatel0045/spring_deployment/SpringDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.smitpatel0045.spring_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smitpatel0045/spring_deployment/SpringDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smitpatel0045.spring_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
